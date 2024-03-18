9c7bcf601e4537d3c7be2bffc9d890cebfbc57ed
Nick Wong <nick.wong@cultureamp.com>
2024-03-18 22:33:06 +1100

    refactor: rename route meta argument

--- app/api/characters/[characterId]/route.ts
+++ app/api/characters/[characterId]/route.ts
@@ -1,23 +1,21 @@
-export const dynamic = 'force-dynamic'
+type RouteMeta = { params: { characterId: string }}
 
-type PageParams = { params: { characterId: string }}
-
-export async function GET(_: Request, { params: { characterId } }: PageParams) {
-  return Response.json({ message: `GET /api/characters/${characterId}` })
+export async function GET(_: Request, meta: RouteMeta) {
+  return Response.json({ message: `GET /api/characters/${meta.params.characterId}` })
 }
 
-export async function POST(_: Request, { params: { characterId } }: PageParams) {
-  return Response.json({ message: `POST /api/characters/${characterId}` })
+export async function POST(_: Request, meta: RouteMeta) {
+  return Response.json({ message: `POST /api/characters/${meta.params.characterId}` })
 }
 
-export async function PUT(_: Request, { params: { characterId } }: PageParams) {
-  return Response.json({ message: `PUT /api/characters/${characterId}` })
+export async function PUT(_: Request, meta: RouteMeta) {
+  return Response.json({ message: `PUT /api/characters/${meta.params.characterId}` })
 }
 
-export async function PATCH(_: Request, { params: { characterId } }: PageParams) {
-  return Response.json({ message: `PATCH /api/characters/${characterId}` })
+export async function PATCH(_: Request, meta: RouteMeta) {
+  return Response.json({ message: `PATCH /api/characters/${meta.params.characterId}` })
 }
 
-export async function DELETE(_: Request, { params: { characterId } }: PageParams) {
-  return Response.json({ message: `DELETE /api/characters/${characterId}` })
+export async function DELETE(_: Request, meta: RouteMeta) {
+  return Response.json({ message: `DELETE /api/characters/${meta.params.characterId}` })
 }
