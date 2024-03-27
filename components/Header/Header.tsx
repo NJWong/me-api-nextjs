91650b3146695e837f803d2ae2497e7d15cb46d9
Nick Wong <njwon4@gmail.com>
2024-03-27 18:08:26 +1100

    feat: basic client auth using auth0

---
+++ components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import HeaderActions from './HeaderActions'
+
+const Header = () => {
+  return (
+    <nav className="flex justify-end px-6 py-4">
+      <HeaderActions />
+    </nav>
+  )
+}
+
+export default Header 
--- env.d.ts
+++ env.d.ts
@@ -3,6 +3,11 @@ declare global {
     interface ProcessEnv {
       TURSO_DATABASE_URL: string;
       TURSO_AUTH_TOKEN: string;
+      AUTH0_SECRET: string;
+      AUTH0_BASE_URL: string;
+      AUTH0_ISSUER_BASE_URL: string;
+      AUTH0_CLIENT_ID: string;
+      AUTH0_CLIENT_SECRET: string;
     }
   }
 }
