25b8cc99ffcdb3207434df913a6eaa32cd5d4966
Nick Wong <njwon4@gmail.com>
2024-03-31 18:22:53 +1100

    feat: GET request for characters

--- app/api/public/characters/route.ts
+++ app/api/public/characters/route.ts
@@ -1,3 +1,12 @@
-export async function GET() {
-  return Response.json({ message: 'GET /api/characters' })
+import { characters } from "@/drizzle/schema"
+import { db } from "@/lib/db"
+
+export async function GET() {  
+  try {
+    const result = await db.select().from(characters).all()
+
+    return Response.json({ status: 200, meta: {}, data: result})
+  } catch {
+    return Response.json({ status: 500, message: 'Internal Server Error' })
+  }
 }
