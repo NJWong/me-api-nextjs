42496fbce9dff95409b208b8f63763e0ce2192c7
Nick Wong <njwon4@gmail.com>
2024-03-31 11:42:37 +1100

    feat: authentication middleware

---
+++ app/api/admin/genders/[id]/route.ts
@@ -0,0 +1,17 @@
+type RouteMeta = { params: { id: string }}
+
+export async function POST(_: Request, meta: RouteMeta) {
+  return Response.json({ message: `POST /api/genders/${meta.params.id}` })
+}
+
+export async function PUT(_: Request, meta: RouteMeta) {
+  return Response.json({ message: `PUT /api/genders/${meta.params.id}` })
+}
+
+export async function PATCH(_: Request, meta: RouteMeta) {
+  return Response.json({ message: `PATCH /api/genders/${meta.params.id}` })
+}
+
+export async function DELETE(_: Request, meta: RouteMeta) {
+  return Response.json({ message: `DELETE /api/genders/${meta.params.id}` })
+}
--- app/api/genders/[id]/route.ts
+++ app/api/genders/[id]/route.ts
@@ -3,19 +3,3 @@ type RouteMeta = { params: { id: string }}
 export async function GET(_: Request, meta: RouteMeta) {
   return Response.json({ message: `GET /api/genders/${meta.params.id}` })
 }
-
-export async function POST(_: Request, meta: RouteMeta) {
-  return Response.json({ message: `POST /api/genders/${meta.params.id}` })
-}
-
-export async function PUT(_: Request, meta: RouteMeta) {
-  return Response.json({ message: `PUT /api/genders/${meta.params.id}` })
-}
-
-export async function PATCH(_: Request, meta: RouteMeta) {
-  return Response.json({ message: `PATCH /api/genders/${meta.params.id}` })
-}
-
-export async function DELETE(_: Request, meta: RouteMeta) {
-  return Response.json({ message: `DELETE /api/genders/${meta.params.id}` })
-}
