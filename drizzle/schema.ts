580d4af72721d6ebe0ecf6721de2ab412ca59667
Nick Wong <njwon4@gmail.com>
2024-03-31 18:22:33 +1100

    feat: drizzle schema for characters, genders and species

--- drizzle/schema.ts
+++ drizzle/schema.ts
@@ -1,3 +1,28 @@
-import { sql } from "drizzle-orm"
 import { text, integer, sqliteTable } from "drizzle-orm/sqlite-core"
 
+export const genders = sqliteTable("genders", {
+  id: integer("id").primaryKey(),
+  name: text("name"),
+})
+
+export type Gender = typeof genders.$inferSelect
+export type NewGender = typeof genders.$inferInsert
+
+export const species = sqliteTable("species", {
+  id: integer("id").primaryKey(),
+  name: text("name"),
+})
+
+export type Species = typeof species.$inferSelect
+export type NewSpecies = typeof species.$inferInsert
+
+export const characters = sqliteTable("characters", {
+  id: integer("id").primaryKey(),
+  name: text("name"),
+  speciesId: integer("species_id").references(() => species.id),
+  genderId: integer("gender_id").references(() => genders.id),
+  class: text("class"), 
+})
+
+export type Character = typeof characters.$inferSelect
+export type NewCharacter = typeof characters.$inferInsert
