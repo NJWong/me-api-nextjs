f695f94186f705f82f2d8ccd1314b9a48f0afe64
Nick Wong <njwon4@gmail.com>
2024-03-31 18:50:57 +1100

    feat: GET character by id

--- app/api/public/characters/[id]/route.ts
+++ app/api/public/characters/[id]/route.ts
@@ -1,5 +1,20 @@
+import { characters } from "@/drizzle/schema"
+import { db } from "@/lib/db"
+import { eq } from "drizzle-orm"
+
 type RouteMeta = { params: { id: string }}
 
 export async function GET(_: Request, meta: RouteMeta) {
-  return Response.json({ message: `GET /api/characters/${meta.params.id}` })
+  try {
+    const id = parseInt(meta.params.id)
+    const result = await db.select().from(characters).where(eq(characters.id, id))
+
+    if (result.length === 0) {
+      return Response.json({ status: 404, message: 'Not Found' })
+    }
+
+    return Response.json({ status: 200, data: result[0] })
+  } catch {
+    return Response.json({ status: 500, message: 'Internal Server Error' })
+  }
 }
