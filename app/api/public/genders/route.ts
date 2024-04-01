80b347e38667e0133ef3507fcc4797236ec63271
Nick Wong <njwon4@gmail.com>
2024-04-01 13:40:18 +1100

    feat: get genders listing

--- app/api/public/genders/route.ts
+++ app/api/public/genders/route.ts
@@ -1,3 +1,24 @@
-export async function GET() {
-  return Response.json({ message: 'GET /api/genders' })
+import { genders } from "@/drizzle/schema"
+import { getPaginationMeta } from "@/helpers/pagination"
+import { db } from "@/lib/db"
+import { count } from "drizzle-orm"
+import { NextRequest } from "next/server"
+
+export async function GET(request: NextRequest) {
+  try {
+    const searchParams = request.nextUrl.searchParams
+    const { limit, offset } = getPaginationMeta(searchParams)
+
+    const total = await db.select({ value: count() }).from(genders)
+    const result = await db.select().from(genders).orderBy(genders.id).limit(limit).offset(offset)
+
+    const data = result.map((gender) => ({
+      id: gender.id,
+      name: gender.name
+    }))
+
+    return Response.json({ status: 200, meta: { total: total[0].value, limit, offset }, data })
+  } catch {
+    return Response.json({ status: 500, message: 'Internal Server Error' })
+  }
 }
