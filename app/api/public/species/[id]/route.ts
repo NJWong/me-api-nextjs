e3157171edc059b02d53009d7f50fcf38732a013
Nick Wong <njwon4@gmail.com>
2024-04-01 17:00:29 +1100

    feat: get species by id

--- app/api/public/species/[id]/route.ts
+++ app/api/public/species/[id]/route.ts
@@ -1,5 +1,25 @@
+import { species } from "@/drizzle/schema"
+import { db } from "@/lib/db"
+import { eq } from "drizzle-orm"
+
 type RouteMeta = { params: { id: string }}
 
 export async function GET(_: Request, meta: RouteMeta) {
-  return Response.json({ message: `GET /api/species/${meta.params.id}` })
+  try {
+    const id = parseInt(meta.params.id)
+    const result = await db.select().from(species).where(eq(species.id, id))
+
+    if (result.length === 0) {
+      return Response.json({ status: 404, message: 'Not Found' })
+    }
+
+    const data = result.map((species) => ({
+      id: species.id,
+      name: species.name,
+    }))[0]
+
+    return Response.json({ status: 200, data })
+  } catch {
+    return Response.json({ status: 500, message: 'Internal Server Error' })
+  }
 }
