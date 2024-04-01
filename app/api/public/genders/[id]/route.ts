675b7f84f0b4543ca9a2a54af72397214c2eaf81
Nick Wong <njwon4@gmail.com>
2024-04-01 17:00:19 +1100

    feat: get gender by id

--- app/api/public/genders/[id]/route.ts
+++ app/api/public/genders/[id]/route.ts
@@ -1,5 +1,25 @@
+import { genders } from "@/drizzle/schema"
+import { db } from "@/lib/db"
+import { eq } from "drizzle-orm"
+
 type RouteMeta = { params: { id: string }}
 
 export async function GET(_: Request, meta: RouteMeta) {
-  return Response.json({ message: `GET /api/genders/${meta.params.id}` })
+  try {
+    const id = parseInt(meta.params.id)
+    const result = await db.select().from(genders).where(eq(genders.id, id))
+
+    if (result.length === 0) {
+      return Response.json({ status: 404, message: 'Not Found' })
+    }
+
+    const data = result.map((gender) => ({
+      id: gender.id,
+      name: gender.name,
+    }))[0]
+
+    return Response.json({ status: 200, data })
+  } catch {
+    return Response.json({ status: 500, message: 'Internal Server Error' })
+  }
 }
