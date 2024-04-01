a54e6606541ee92d732d2fa72c41c1f33a462687
Nick Wong <njwon4@gmail.com>
2024-04-01 13:40:28 +1100

    feat: get species listing

--- app/api/public/species/route.ts
+++ app/api/public/species/route.ts
@@ -1,3 +1,24 @@
-export async function GET() {
-  return Response.json({ message: 'GET /api/species' })
+import { species } from "@/drizzle/schema";
+import { getPaginationMeta } from "@/helpers/pagination";
+import { db } from "@/lib/db";
+import { count } from "drizzle-orm";
+import { NextRequest } from "next/server";
+
+export async function GET(request: NextRequest) {
+  try {
+    const searchParams = request.nextUrl.searchParams
+    const { limit, offset } = getPaginationMeta(searchParams)
+
+    const total = await db.select({ value: count() }).from(species)
+    const result = await db.select().from(species).orderBy(species.id).limit(limit).offset(offset)
+
+    const data = result.map((species) => ({
+      id: species.id,
+      name: species.name
+    }))
+
+    return Response.json({ status: 200, meta: { total: total[0].value, limit, offset }, data})
+  } catch {
+    return Response.json({ status: 500, message: 'Internal Server Error' })
+  }
 }
